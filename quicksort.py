41cbbeaae03bee3696135d7fa79f7434ee8fe149
Michael Williams <mike@pentangle.net>
2014-05-25 20:34:58 -0400

    quicksort tests in python

--- quicksort.py
+++ quicksort.py
@@ -32,19 +32,29 @@ def partition(B, p):
 
 def quicksort(A, comp=0):
     '''
-    >>> quicksort([3, 9, 8, 4, 6, 10, 2, 5, 7, 1])
-    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 25)
+    >>> quicksort([3, 8, 2, 5, 1, 4, 7, 6])
+    ([1, 2, 3, 4, 5, 6, 7, 8], 25)
     '''
     if len(A) <= 1:
         return (A, comp)
     else:
         comp += len(A) - 1
-        p = choosepivot_last(A)
+        p = choosepivot_first(A)
         p_value = A[p]
         (A1, A2) = partition(A, p)
         A1, comp = quicksort(A1, comp=comp)
         A2, comp = quicksort(A2, comp=comp)
         return (A1 + [p_value] + A2, comp)
+
+
+def test_quicksort():
+    import mergesort
+    for n in [10]:
+        f = open("quicksort_tests/" + str(n) + ".txt", "r")
+        A = [int(i) for i in f]
+        A, comp = quicksort(A)
+        assert mergesort.mergesort(A) == A
+        print comp
 
 
 if __name__ == "__main__":
