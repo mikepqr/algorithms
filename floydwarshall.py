b312458a033362557d20ebf6fd9a9bd83600e428
Mike Williams <mike@pentangle.net>
2015-04-27 02:07:12 -0400

    Add comments

--- floydwarshall.py
+++ floydwarshall.py
@@ -24,25 +24,53 @@ def convert_weighted_edge_list_to_adjacency_list(edges, directed=True):
 
 
 def floydwarshall(n, edges):
+    '''
+    Returns the minimum shortest path in a directed graph with n nodes and edge
+    list edges. Uses the Floyd Warshall algorithm.
+    '''
     g = convert_weighted_edge_list_to_adjacency_list(edges)
-    A = np.zeros((n+1, n+1, n+1), dtype=np.int) + 200000
+
+    # A is an n+1 x n+1 x n+1 array. The first dimension is indexed by k. Each
+    # n+1 x n+1 array at each k is the minimum shortest path from node i to
+    # node j. The values for i = 0 or j = 0 are undefined for all k. The
+    # redundant 0th row/column simplifies dealing with the conventional input
+    # for this problem, in which nodes are numbered 1 to n. Effectivtly it
+    # turns Python into a 1-indexing language.
+    #
+    # int(2**31 - 1) is an arbitrary large integer (the largest possible in 32
+    # bit precision), and denotes the absence of a path between two nodes
+    # (which is true for all pairs of nodes not connected by 1 edge, at the
+    # beginning of the code).
+    A = np.zeros((n+1, n+1, n+1), dtype=np.int) + int(2**31 - 1)
+    # Read in the edges.
     for u, vs in g.items():
         for v in vs:
             A[0, u, v[0]] = v[1]
+    # Nodes are connected to themselves.
     for i in range(1, n+1):
         A[0, i, i] = 0
 
+    # The Floyd Warshall considers subproblems of size k, where k is the
+    # maximum number of edges that can be used to build a path from node i to
+    # j. The best possible path from i to j using up to k edges is then the
+    # better of:
+    #  1. The path from i to j using up to k - 1 edges, and
+    #  2. The path from i to w using up to k - 1 edges + the path from w to j,
+    #     for all nodes w that have edges leading directly to j
     for k in range(1, n+1):
-        # Matrix-manipulation speedup:
+        # Matrix-manipulation trick:
         #   X[i, k] + Y[k, j] = Z[i, j] if
         #   Z = X[k, :] + A[:, k]_transpose
         # Where the calculation of the matrix Z uses broadcasting rather than a
-        # double for loop
+        # double for loop. The (n+1, n+1) array B is then case 2 above
         B = A[k-1, k, :] + A[k-1, :, k][np.newaxis].T
         A[k, :, :] = np.minimum(A[k - 1, :, :], B)
 
+    # Floyd Warshall has at least one negative number on the leading diagonal
+    # is a negative weight closed cycle was found (in which case the minimum
+    # shortest path is not defined).
     for i in range(1, n+1):
         if A[n, i, i] < 0:
-            return np.inf
+            return -np.inf
     else:
         return np.min(A[n, :, :])
