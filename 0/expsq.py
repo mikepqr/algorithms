76f06103b3854ac0f19e62fa6cf9101063d4b489
Mike Williams <mike@mike.place>
2016-12-18 15:05:58 -0500

    Use 0-index counting of bits

--- 0/expsq.py
+++ 0/expsq.py
@@ -7,18 +7,20 @@ def standingbits(n):
     '''
     Return a list containing the offsets of the non-zero bits of int n.
 
-    >>> standingbits(12) # 0b1100
-    [3, 4]
+    >>> standingbits(12) # 0b1100 = 2^3 + 2^2
+    [2, 3]
+    >>> standingbits(25) # 0b11001 = 2^4 + 2^3 + 2^0
+    [0, 3, 4]
     '''
     # 12 = 0b1100 -> [0, 0, 1, 1]
+    # 25 = 0b11001 -> [1, 0, 0, 1, 1]
     # For each bit from first to length of the number in binary
-    # - shift the binary number right by that many bits, 1100 >> 2 = 0011
-    # - & that with 0001, which yields 0 if the bit is 0, 1 if 1
-    bits = [(n >> bit) & 1 for bit in range(n.bit_length())]
+    # - shift the number right by that many bits, e.g. 1100 >> 2 = 0011
+    # - & the result with 1, which yields 0 if the rightmost bit is 0, 1 if 1
+    bits = [(n >> offset) & 1 for offset in range(n.bit_length())]
 
-    # Return corresponding numbers, accounting for fact that 0th element of
-    # bits corresponds to 1, 1st element = 2, etc.
-    return [i + 1 for i, v in enumerate(bits) if v]
+    # Return locations of 1s in bits
+    return [i for i, b in enumerate(bits) if b]
 
 
 def expsq(x, n):
@@ -40,6 +42,6 @@ def expsq(x, n):
 
     # Pluck out elements of list that are standing bits of n.
     # e.g. for n=25 this gives [x^1, x^8, x^16]
-    squares = (s for i, s in enumerate(allsquares) if i+1 in standingbits(n))
+    squares = (s for i, s in enumerate(allsquares) if i in standingbits(n))
 
     return functools.reduce(operator.mul, squares)
