5f9858ccf07da900d7dbbc4075d07ba7e0b23550
Mike Lee Williams <mike@mike.place>
2017-01-12 15:37:57 -0500

    Raise error if message too large for key

--- 1/rsa.py
+++ 1/rsa.py
@@ -1,7 +1,7 @@
 import arithmetic
 
 
-def twoprimes(n=100, p=None, q=None):
+def twoprimes(p=None, q=None, n=100):
     '''
     Returns two different primes up to n bits long.
 
@@ -23,14 +23,14 @@ def twoprimes(n=100, p=None, q=None):
     return p, q
 
 
-def keypair(n=100, p=None, q=None):
+def keypair(p=None, q=None, n=100):
     '''
     Return a valid RSA public (N, e) and private (N, d) keypair.
 
     If p and/or q are set they are used as the primes to generate the keys.
     Otherwise primes up to n bits long are randomly generated.
     '''
-    p, q = twoprimes(n=n, p=p, q=q)
+    p, q = twoprimes(p=p, q=q, n=n)
     pq = arithmetic.mult(p, q)
     p1q1 = arithmetic.mult(p - 1, q - 1)
 
@@ -50,6 +50,8 @@ def keypair(n=100, p=None, q=None):
 
 def encode(x, public):
     N, e = public
+    if x >= N:
+        raise ValueError('Message x={} too large for key N={}'.format(x, N))
     return arithmetic.modexp(x, e, N)
 
 
