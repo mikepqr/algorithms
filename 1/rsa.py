40e849f4e6ced10f9397d12ec1f744755f9b8d27
Mike Lee Williams <mike@mike.place>
2017-01-12 17:50:49 -0500

    Add functions to rsa encode/decode unicode strings

--- 1/rsa.py
+++ 1/rsa.py
@@ -1,5 +1,8 @@
 import arithmetic
 
+LARGEST_UNICODE_POINT = 0x10FFFF
+cpsize = len(str(LARGEST_UNICODE_POINT))  # codepoint size
+
 
 def twoprimes(p=None, q=None, n=100):
     '''
@@ -48,13 +51,28 @@ def keypair(p=None, q=None, n=100):
     return public, private
 
 
-def encode(x, public):
+def encode_num(x, public):
     N, e = public
     if x >= N:
         raise ValueError('Message x={} too large for key N={}'.format(x, N))
     return arithmetic.modexp(x, e, N)
 
 
-def decode(x, private):
+def decode_num(x, private):
     N, d = private
     return arithmetic.modexp(x, d, N)
+
+
+def encode(clear_message, public):
+    ords = [ord(m) for m in clear_message]
+    ords_str = ''.join(str(o).zfill(cpsize) for o in ords)
+    # prepend arbitrary non-zero digit to ensure leading 0s are not lost
+    x = int('9' + ords_str)
+    return encode_num(x, public)
+
+
+def decode(encrypted_message, private):
+    x = str(decode_num(encrypted_message, private))
+    # chunk decoded string into cpsize strings, skipping arbitrary digit
+    ords = [int(x[i:i+cpsize]) for i in range(1, len(x), cpsize)]
+    return ''.join(chr(o) for o in ords)
